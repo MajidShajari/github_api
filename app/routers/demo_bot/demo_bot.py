73bb464066ac90494291de8fe7cd04aa7ef0847a
Majid Shajari <majidpy89@gmail.com>
2023-07-09 01:40:22 +0330

    Update Favicon

--- app/routers/demo_bot/demo_bot.py
+++ app/routers/demo_bot/demo_bot.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Request
 
 router = APIRouter(prefix="/shijarbot", tags=["Shijar Bot"])
+
+
 @router.get("/")
 async def root(request: Request):
     response = await request.json()
