3e3850a066e31d5ab50f0a5b4892c4f135d91084
Majid Shajari <majidpy89@gmail.com>
2023-07-09 08:24:47 +0330

    Update Telegram Route

--- app/routers/demo_bot/demo_bot.py
+++ app/routers/demo_bot/demo_bot.py
@@ -3,7 +3,12 @@ from fastapi import APIRouter, Request
 router = APIRouter(prefix="/shijarbot", tags=["Shijar Bot"])
 
 
-@router.get("/")
+@router.post("/")
 async def root(request: Request):
     response = await request.json()
     print(response["message"]["text"])
+
+
+@router.get("/")
+async def demo_bot():
+    return {"message": "Telegram Webhook"}
